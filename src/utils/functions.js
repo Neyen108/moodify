150de412254c53724bdcc54a1d153abcc15cc8c8
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-03-11 21:53:16 +0530

    add api call functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//get the access token from the url
 export const getParamValues = (url) => {
   return url
     .slice(1)
@@ -9,4 +10,77 @@ export const getParamValues = (url) => {
       prev[title] = value;
       return prev;
     }, {});
+};
+
+//set the header for all requests to the spotify API
+const setAuthHeader = (accessToken) => {
+  try {
+    if (accessToken) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    }
+  } catch (error) {
+    console.log('Error setting Auth Header', error);
+  }
+};
+
+//generalized spotify GET request
+const spotifyGetRequest = async (url, accessToken) => {
+  //set the access token header
+  setAuthHeader(accessToken);
+  //get result
+  const result = await axios.get(url);
+  return result.data;
+};
+
+//GET the analysis of the songs
+const getSongsAnalysis = async (ids, accessToken) => {
+  const songsAnalysis = await spotifyGetRequest(
+    `https://api.spotify.com/v1/audio-features/?ids=${ids}`,
+    accessToken
+  );
+
+  return songsAnalysis;
+};
+
+//Get user profile
+export const getUserProfile = async (accessToken) => {
+  const userProfile = await spotifyGetRequest(
+    'https://api.spotify.com/v1/me/',
+    accessToken
+  );
+  return userProfile;
+};
+
+//get recently played tracks
+export const getRecentlyPlayedTracks = async (accessToken) => {
+  const recentlyPlayedTracks = await spotifyGetRequest(
+    `https://api.spotify.com/v1/me/player/recently-played`,
+    accessToken
+  );
+
+  return recentlyPlayedTracks.items;
+};
+
+//get Song Analysis from the song array
+export const getSongsAnalysisArray = async (songArr) => {
+  //create Array of only Song IDS
+  const ids = songArr.map((song) => {
+    return song.track.id;
+  });
+  const stringIds = ids.toString();
+
+  //get analysis from Spotify API
+  const data = await getSongsAnalysis(stringIds);
+  const audioData = data.audio_features;
+
+  //Add song properties (name/album/etc) from initial song array
+  for (let i = 0; i < audioData.length; i++) {
+    audioData[i].name = songArr[i].track.name;
+    audioData[i].album = songArr[i].track.album.name;
+    audioData[i].artist = songArr[i].track.artists[0].name;
+    audioData[i].imageURL = songArr[i].track.album.images[1].url;
+    audioData[i].playedAt = songArr[i].played_at;
+    audioData[i].countIndex = i + 1;
+  }
+  return audioData;
 };
