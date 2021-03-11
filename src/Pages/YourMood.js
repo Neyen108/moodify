4c3b2171c4c32c12fc3b4a2a52fc292341e531a4
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-03-12 02:55:05 +0530

    add laypot for displaying mood

--- src/Pages/YourMood.js
+++ src/Pages/YourMood.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {
+  getRecentlyPlayedTracks,
+  getSongsAnalysisArray,
+  getUserProfile,
+} from '../utils/functions';
+import { connect } from 'react-redux';
+import { setTokens } from '../actions';
+
+import Navbar from '../components/Navbar';
+import Loading from '../components/Loading';
 
 const YourMood = () => {
   return <h1>Logged In</h1>;
 };
 
-export default YourMood;
+const mapStateToProps = (state) => {
+  return { tokens: state.tokens };
+};
+
+export default connect(mapStateToProps, { setTokens })(YourMood);
---
+++ src/components/AnalyzingContainer.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import AnalyzingDisplay from './AnalyzingDisplay';
+import Loading from './Loading';
+import MoodDisplay from './MoodDisplay';
+
+const AnalyzingContainer = ({ songsArray, userProfile }) => {
+  const [currentSong, setCurrentSong] = useState('');
+  const [finishedAnalysis, setFinishedAnalysis] = useState(false);
+
+  useEffect(() => {
+    songsArray.forEach((song, i) => {
+      setTimeout(() => {
+        setCurrentSong(song);
+      }, i * 150);
+    });
+
+    setTimeout(() => {
+      setFinishedAnalysis(true);
+    }, 3150);
+  }, []);
+
+  //first wait for async/await to finish
+  if (finishedAnalysis === false && songsArray.length === 0) {
+    return (
+      <div>
+        <Loading text={'Connecting to Spotify....'} />
+      </div>
+    );
+  }
+
+  //iterate through the songs being analysed
+  if (finishedAnalysis === false && songsArray.length > 0) {
+    return <AnalyzingDisplay song={currentSong} />;
+  }
+
+  //when analysis is finished return the mood
+  return (
+    <div>
+      <MoodDisplay songs={songsArray} profile={userProfile} />
+    </div>
+  );
+};
+
+export default AnalyzingContainer;
